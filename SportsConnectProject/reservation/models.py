5273385b136a1cd280448cad38c017419e75cd70
ACarmona4 <acarmonas@eafit.edu.co>
2024-08-10 18:39:51 -0500

    Cambio en models

--- SportsConnectProject/reservation/models.py
+++ SportsConnectProject/reservation/models.py
@@ -7,7 +7,7 @@ class User(models.Model):
     name = models.CharField(max_length=20)
     lastname1 = models.CharField(max_length=20)
     lastname2 = models.CharField(max_length=20)
-    email = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
     phone = models.IntegerField(max_length=10)
 
 
