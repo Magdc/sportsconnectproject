1e869ddd6f6b11e8180c03e9862ebe120821f7e2
Santiago <santiagord2004@gmail.com>
2024-09-08 21:10:09 -0500

    Solucion redireccionamiento y personalizacion navbar

--- SportsConnectProject/SportsConnect/settings.py
+++ SportsConnectProject/SportsConnect/settings.py
@@ -127,6 +127,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "SportsConnect"),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
